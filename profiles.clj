b6c9c8ac4fa161b5f3923b6c461020ebb8afe540
Mac Heller-Ogden <mac@machellerogden.com>
2018-11-28 11:08:32 -0600

    update

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,7 @@
 {:user {:plugins [[cider/cider-nrepl "0.17.0"]
                   [org.clojars.timvisher/lein-checkout "0.4.2"]
-                  [lein-sub "0.3.0"]]
+                  [lein-sub "0.3.0"]
+                  [lein-kibit "0.1.5"]]
         :dependencies [[cljfmt "0.5.1"]
                        [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]]
         :checkout {:search-roots ["/Users/mogden/repos/github.com/Guaranteed-Rate"
